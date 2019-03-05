82d0c5ea9abcfc5caa9d10a4f12335c4c025ed18
Danilo C. Graciano <danilocgraciano@gmail.com>
2019-03-05 14:50:56 -0300

    Nova rota para api gateway.

---
+++ api-gateway/app/routes/order.js
@@ -0,0 +1,19 @@
+const httpProxy = require('express-http-proxy');
+const serviceProxy = httpProxy('http://localhost:8083');
+
+let proxyFunction = (req, res, next) => {
+    serviceProxy(req, res, next);
+};
+
+module.exports = function (app) {
+
+    app.route('/orders')
+        .get(proxyFunction)
+        .post(proxyFunction);
+
+    app.route('/orders/:id')
+        .get(proxyFunction)
+        .put(proxyFunction)
+        .delete(proxyFunction);
+
+};
